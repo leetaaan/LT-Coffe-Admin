db75c2c18c7752126201545e82edc938a2d864ae
leetaaan <119284828+leetaaan@users.noreply.github.com>
2024-10-17 20:50:45 +0700

    admin-lt-coffee-updated

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASUFv5b9eW2cQTdIAEsYT5b7WaK_FahfY",
-  authDomain: "food-app-d6c4e.firebaseapp.com",
-  projectId: "food-app-d6c4e",
-  storageBucket: "food-app-d6c4e.appspot.com",
-  messagingSenderId: "406041558048",
-  appId: "1:406041558048:web:39454bd9ab8dedd39b5045",
-  measurementId: "G-7L21CSFD6B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 console.log(firebaseConfig);
 
